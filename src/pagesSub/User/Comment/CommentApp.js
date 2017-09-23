29ccb0315cb3f3f9f9a6b0e3807ad255b287a12c
v_ganyanlin <v_ganyanlin@baidu.com>
2017-09-23 18:56:35 +0800

    提交

--- src/pagesSub/User/Comment/CommentApp.js
+++ src/pagesSub/User/Comment/CommentApp.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
-// const writeFileP = require('write-file-p');
-var fs = require('fs');
 
 // const comments = [
 //     {username: 'Jerry', content: '练兵备战强势推进，训战一体疾风劲吹', date: 15000020338353},
@@ -24,7 +22,7 @@ class CommentApp extends Component {
     componentDidMount() {
         //读取文件/接口
         fetch('./data/Comment.json', {
-            headers : {
+            headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             }
@@ -37,24 +35,19 @@ class CommentApp extends Component {
             });
     };
 
-    handleSubmitComment (comment) {
+    handleSubmitComment(comment) {
         if (!comment) return;
         if (!comment.username) return alert('请输入用户名');
         if (!comment.content) return alert('请输入评论内容');
-        // writeFileP.sync(`./data/Comment.json`, {
-        //     username: comment.username,
-        //     content: comment.content,
-        //     date: comment.date
-        // });
-        fs.writeFileSync('./data/Comment.json', 'Hello Node');
+
         this.state.comments.push(comment);
         this.setState({
             comments: this.state.comments
         });
     }
 
-    handleClose(closeId){
-        this.state.comments.splice(closeId.i,1);
+    handleClose(closeId) {
+        this.state.comments.splice(closeId.i, 1);
         this.setState({
             comments: this.state.comments
         });
